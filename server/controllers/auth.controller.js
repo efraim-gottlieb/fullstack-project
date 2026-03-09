46c0d79866f76df0aab84f420a41996109748b7a
EfraimGottlieb <efgo583208979@gmail.com>
2026-03-09 12:51:55 +0200

    fix  login logic

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -6,7 +6,7 @@ export async function login(req, res) {
   const { username, password } = req.body;
   const users = await getUsers();
   const user = users.find((u) => u.fullName == username);
-  if (user && user.password == password) {
+  if (user || user.password == password) {
     res.send(generateToken(user));
   } else {
     res.status(403).end("Unauthorized");
